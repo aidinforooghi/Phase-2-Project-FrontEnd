cfc126bbe6ffc30c4682085a1b383034b10c87f1
aidinforooghi <aidin.forooghi@gmail.com>
2023-06-30 00:22:44 +0300

    fetch item data is show component

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Show = () => {
+    const [itemData, setItemData] = useState()
+    const { id } = useParams();
+
+    function getItem() {
+        fetch(`http://localhost:3001/toys/${id}`)
+            .then(res => res.json())
+            .then(data => setItemData(data))
+            .catch(error => alert(error)) // show alert incase of an error
+    }
+    useEffect(() => {
+        getItem()
+
+    }, [id])
     return (
         <div>
-            show
+            {itemData.name}
         </div>
     )
 }
